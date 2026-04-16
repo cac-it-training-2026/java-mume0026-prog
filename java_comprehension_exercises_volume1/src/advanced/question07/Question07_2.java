2f67b225cd420bf69a7de72a44fa8e8a36cb24be
miraiu <miraiu@C010423022.zipangu.cac.co.jp>
2026-04-16 16:50:05 +0900

    Question0703まで完了

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
@@ -1,17 +1,46 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_2 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		// 登録済み商品（値は変更可）
 		String[] itemNames = { "ノート", "ボールペン", "消しゴム", "はさみ", "のり" };
 		// 単価（値は変更可）
 		int[] prices = { 100, 200, 300, 400, 500 };
+		int[] buyCounts = new int[5];
 
-		// TODO: 実装ここから
-		
+		//		for (int i = 0; i < 5; i++) {
+		//			System.out.println(itemNames[i] + "の在庫数:>" + stocks[i]);
+		//
+		//		}
+
+		for (int i = 0; i < 5; i++) {
+
+			System.out.println(itemNames[i] + "の購入数>");
+			String str1 = reader.readLine();
+			int buy = Integer.parseInt(str1);
+
+			buyCounts[i] = buy;
+		}
+		//			if (buyCounts[i] > stocks[i]) {
+		//				System.out.println("在庫数を超えています。もう一度入力してください。");
+		//				i--;
+		//			} else {
+		//				break;
+		int Totalprices = 0;
+		for (int j = 0; j < 5; j++) {
+			System.out.println(itemNames[j] + ":" + (prices[j] * buyCounts[j] + "円"));
+			Totalprices += (buyCounts[j] * prices[j]);
+		}
+
+		System.out.println("合計金額:" + Totalprices + "円");
+
 	}
+
 }
