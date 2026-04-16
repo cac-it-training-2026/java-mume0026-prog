25a3cfc8b4cbad60f61e72995ef1deb3d989b042
miraiu <miraiu@C010423022.zipangu.cac.co.jp>
2026-04-16 13:27:14 +0900

    Question2まで完了

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -10,7 +12,16 @@ public class Question02_1 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("名前を入力してください。");
+		String name = reader.readLine();
+
+		System.out.println("\n好きなスポーツを入力してください。");
+		String sports = reader.readLine();
+
+		System.out.println("\n私の名前は" + name + "です。");
+		System.out.println("好きなスポーツは" + sports + "です。");
 
 	}
 }
